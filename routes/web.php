48f7ef3193c3796f45a323eb3c59eb77a856df68
Jose <jaabellanmotos@gmail.com>
2019-07-26 12:14:59 +0200

    route post added

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,15 @@ Route::get('/', function () {
 Route::get('/submit', function (){
     return view('submit');
 });
+Route::post('/submit', function(Request $request){
+    $data=$request->validate([
+        'title'=>'required|max:255',
+        'url'=>'required|url|max:255',
+        'description'=>'required|max:255',
+    ]);
+    $link=tap(new App\Link($data))->save();
+    return redirect('/');
+});
 
 Auth::routes();
 
